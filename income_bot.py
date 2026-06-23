acddeef560676b305b33e37b24b9b2506dcb189c
2zycky946f-cell <2zycky946f@privaterelay.appleid.com>
2026-06-23 00:16:09 -0700

    Update income_bot.py

--- income_bot.py
+++ income_bot.py
@@ -1,12 +1,23 @@
 import os
 import re
-import asyncio
 import sqlite3
+import asyncio
 import aiohttp
-import easyocr 
+import easyocr
 
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, BotCommand
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+
+print("Loading OCR...")
+
+ocr = easyocr.Reader(
+    ["en"],
+    gpu=False,
+    verbose=False
+)
+
+print("OCR Ready")
+print("OCR OBJECT CREATED")
 
 TOKEN = os.getenv("BOT_TOKEN")
 CENSUS_KEY = os.getenv("CENSUS_API_KEY")
